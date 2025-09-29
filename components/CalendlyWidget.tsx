36af2e2f416d94834be19253c3329dc97d45d02b
Firmin THIERY <52818703+Firemain@users.noreply.github.com>
2025-09-29 16:21:22 +0200

    Updated CalendlyWidget.tsx

--- components/CalendlyWidget.tsx
+++ components/CalendlyWidget.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { useEffect } from 'react'
 import Script from 'next/script'
 
 export default function CalendlyWidget() {
-  useEffect(() => {
-    // Rien à faire ici, Calendly charge automatiquement
-  }, [])
-
   return (
-    <div className="calendly-inline-widget"
-         data-url="https://calendly.com/87fugiii/30min"
-         style={{ minWidth: '320px', height: '700px' }} />
+    <>
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/87fugiii/30min"
+        style={{ minWidth: '320px', height: '700px' }}
+      />
+      <Script
+        src="https://assets.calendly.com/assets/external/widget.js"
+        strategy="afterInteractive"
+      />
+    </>
   )
 }
