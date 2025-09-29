a76de07cb66aa1e77d2742333b1e205c19ff8063
Firmin THIERY <52818703+Firemain@users.noreply.github.com>
2025-09-29 17:32:31 +0200

    Updated Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,6 @@ export default function Header() {
     { name: 'Réalisations', href: '/realisations' },
     { name: 'Formations', href: '/formations' },
     { name: 'Légal', href: '/legal' },
-    { name: 'Contact', href: '/contact' },
   ]
 
   return (
