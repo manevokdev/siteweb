947ed301a98c586d1d02f8dc67a7c5fbbca34875
Firemain <87fugiii@gmail.com>
2025-11-04 17:19:02 +0100

    fix email

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
     // Email de notification pour le client (Olivier)
     const notificationEmail = await resend.emails.send({
       from: 'MANévok <contact@manevok.com>',
-      to: 'thry.firmin@gmail.com',
+      to: 'olivier.kunstmann@outlook.fr',
       subject: `Nouveau message de ${name}`,
       html: `
         <!DOCTYPE html>
