7f0e6a5f3875db9850edc435d20086782b4d450d
Firemain <87fugiii@gmail.com>
2025-11-04 18:29:36 +0100

    calendly

--- components/CalendlyWidget.tsx
+++ components/CalendlyWidget.tsx
@@ -7,7 +7,7 @@ export default function CalendlyWidget() {
     <>
       <div
         className="calendly-inline-widget"
-        data-url="https://calendly.com/87fugiii/30min"
+        data-url="https://calendly.com/olivier-kunstmann/30min"
         style={{ minWidth: '320px', height: '700px' }}
       />
       <Script
